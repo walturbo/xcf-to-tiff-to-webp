60631ea548d6314e1903acd5d1a1134365895237
walturbo <walter.consoli@gmail.com>
2026-02-11 16:36:03 +0100

    Create main_window.py for image converter GUI

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,74 +1,31 @@
 import sys
-from PyQt6 import QtWidgets, QtCore
-from PyQt6.QtWidgets import QTabWidget, QWidget, QVBoxLayout, QPushButton, QLabel, QFileDialog, QProgressBar, QLineEdit
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QFileDialog, QLabel
 
-class MainWindow(QtWidgets.QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self):
-        super(MainWindow, self).__init__()
-        self.setWindowTitle('XCF to TIFF to WebP Converter')
-        self.setGeometry(100, 100, 600, 400)
+        super().__init__()  
+        self.setWindowTitle("Image Converter")
 
-        # Creating tabs
-        self.tabs = QTabWidget()
-        self.setCentralWidget(self.tabs)
+        self.layout = QVBoxLayout()
+        self.label = QLabel("Select an image to convert:")
+        self.layout.addWidget(self.label)
 
-        self.conversion_tab = QWidget()
-        self.about_tab = QWidget()
+        self.button = QPushButton("Choose File")
+        self.button.clicked.connect(self.open_file_dialog)
+        self.layout.addWidget(self.button)
 
-        self.tabs.addTab(self.conversion_tab, 'Convert')
-        self.tabs.addTab(self.about_tab, 'About')
+        self.container = QWidget()  
+        self.container.setLayout(self.layout)
+        self.setCentralWidget(self.container)
 
-        self.setup_conversion_tab()
-        self.setup_about_tab()
-
-    def setup_conversion_tab(self):
-        layout = QVBoxLayout()
-
-        self.file_path = QLineEdit()
-        self.file_path.setPlaceholderText('Select XCF file')
-        layout.addWidget(self.file_path)
-
-        self.select_file_button = QPushButton('Browse')
-        self.select_file_button.clicked.connect(self.select_file)
-        layout.addWidget(self.select_file_button)
-
-        self.quality_label = QLabel('Quality (1-100):')
-        layout.addWidget(self.quality_label)
-
-        self.quality_input = QLineEdit()
-        self.quality_input.setPlaceholderText('Quality Setting')
-        layout.addWidget(self.quality_input)
-
-        self.convert_button = QPushButton('Convert to TIFF & WebP')
-        self.convert_button.clicked.connect(self.convert_files)
-        layout.addWidget(self.convert_button)
-
-        self.progress_bar = QProgressBar()
-        layout.addWidget(self.progress_bar)
-
-        self.conversion_tab.setLayout(layout)
-
-    def setup_about_tab(self):
-        layout = QVBoxLayout()
-        about_label = QLabel('XCF to TIFF to WebP Converter\nCreated by Walturbo\nDate: 2026-02-11')
-        layout.addWidget(about_label)
-        self.about_tab.setLayout(layout)
-
-    def select_file(self):
-        file_name, _ = QFileDialog.getOpenFileName(self, "Select XCF File", "", "XCF Files (*.xcf)")
+    def open_file_dialog(self):
+        options = QFileDialog.Options()
+        file_name, _ = QFileDialog.getOpenFileName(self, "Select Image File", "", "Images (*.png *.xpm *.jpg *.tiff *.webp);;All Files (*)", options=options)
         if file_name:
-            self.file_path.setText(file_name)
-
-    def convert_files(self):
-        # Here you would implement the actual conversion logic
-        self.progress_bar.setValue(0)  # Reset progress bar
-        # Dummy progress loop
-        for i in range(101):
-            self.progress_bar.setValue(i)
-            QtCore.QThread.msleep(50)  # Simulate time delay
+            self.label.setText(f"Selected: {file_name}")
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    main_window = MainWindow()
-    main_window.show()
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
     sys.exit(app.exec())
